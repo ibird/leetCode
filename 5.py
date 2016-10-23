da9732be42899c496317418708342e222e1f85e2
shaopeng.yuan <shaopeng.yuan@reorientgroup.com>
2016-10-23 19:02:04 +0800

    总算把第五题做出来了，算法不强的时候应该多点耐心

---
+++ 5.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+
+class Solution(object):
+    def getPalindrome(self, s, i, totalLen):
+        if i != 1 or i != totalLen:
+            left = i - 1
+            right = totalLen - i
+            f = None
+
+            for j in xrange(min(left, right)):
+                if s[i - j - 2] != s[i + j]:
+                    break
+                else:
+                    f = j
+            if f is not None:
+                palindrome1 = s[i - f - 2: i + f + 1]
+            else:
+                palindrome1 = s[i - 1]
+
+        left = i
+        right = totalLen - i
+        f = None
+        palindrome2 = ""
+        for j in xrange(min(left, right)):
+            if s[i - j - 1] != s[i + j]:
+                break
+            else:
+                f = j
+            if f is not None:
+                palindrome2 = s[i - f - 1: i + f + 1]
+            else:
+                palindrome2 = ""
+
+        return palindrome1 if len(palindrome1) > len(palindrome2) else palindrome2
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longest = ""
+        lenstr = len(s)
+
+        if lenstr == 0:
+            return ""
+        if lenstr == 1:
+            return s
+
+        for i in xrange(1, lenstr):
+            palindrome = self.getPalindrome(s, i, lenstr)
+            longest = palindrome if len(palindrome) > len(longest) else longest
+        return longest
+
+
+if __name__ == "__main__":
+    # s = "abb"
+    # s = "ccc"
+    # s = "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+    # s = "jrjnbctoqgzimtoklkxcknwmhiztomaofwwzjnhrijwkgmwwuazcowskjhitejnvtblqyepxispasrgvgzqlvrmvhxusiqqzzibcyhpnruhrgbzsmlsuacwptmzxuewnjzmwxbdzqyvsjzxiecsnkdibudtvthzlizralpaowsbakzconeuwwpsqynaxqmgngzpovauxsqgypinywwtmekzhhlzaeatbzryreuttgwfqmmpeywtvpssznkwhzuqewuqtfuflttjcxrhwexvtxjihunpywerkktbvlsyomkxuwrqqmbmzjbfytdddnkasmdyukawrzrnhdmaefzltddipcrhuchvdcoegamlfifzistnplqabtazunlelslicrkuuhosoyduhootlwsbtxautewkvnvlbtixkmxhngidxecehslqjpcdrtlqswmyghmwlttjecvbueswsixoxmymcepbmuwtzanmvujmalyghzkvtoxynyusbpzpolaplsgrunpfgdbbtvtkahqmmlbxzcfznvhxsiytlsxmmtqiudyjlnbkzvtbqdsknsrknsykqzucevgmmcoanilsyyklpbxqosoquolvytefhvozwtwcrmbnyijbammlzrgalrymyfpysbqpjwzirsfknnyseiujadovngogvptphuyzkrwgjqwdhtvgxnmxuheofplizpxijfytfabx"
+    # s = "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+    s = "abb"
+    print Solution().longestPalindrome(s)
